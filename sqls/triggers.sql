47674d6c6d7a1e7803103a42349700192fd6f7b5
welllet <welllet@mail.ru>
2022-11-21 21:13:45 +0300

    added test function

---
+++ sqls/triggers.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION update_department_money(cost integer, department_id integer) RETURNS integer
+LANGUAGE
+plpgsql
+AS
+$$
+begin
+	UPDATE
+	department
+	SET
+	current_resource = cost
+	WHERE
+	department.id = department_id;
+end;
+$$
+
