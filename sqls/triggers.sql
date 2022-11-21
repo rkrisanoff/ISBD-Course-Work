fbf1b428893f9111ab1bb6afd09f85856ac17e54
welllet <welllet@mail.ru>
2022-11-21 21:41:06 +0300

    added trigger

--- sqls/triggers.sql
+++ sqls/triggers.sql
@@ -1,16 +1,39 @@
-CREATE FUNCTION update_department_money(cost integer, department_id integer) RETURNS integer
+CREATE FUNCTION update_department_resource(task_id integer) RETURNS integer
 LANGUAGE
 plpgsql
 AS
 $$
+DECLARE
+	task_cost integer;
 begin
+	SELECT
+	cost
+	INTO
+	task_cost
+	FROM
+	task
+	WHERE
+	task.id = task_id;
+
 	UPDATE
 	department
 	SET
-	current_resource = cost
+	current_resource = task_cost
 	WHERE
-	department.id = department_id;
+	department.id = (
+		SELECT
+		department.id
+		FROM
+		task
+		JOIN post ON task.executor_id = post.employee_id and task.id = task_id
+		JOIN department ON post.department.id = post.department_id
+	)
 	RETURN 0;
 end;
 $$
 
+CREATE TRIGGER task_update
+	AFTER UPDATE OF state ON task
+	FOR EACH ROW
+	WHEN (NEW.state = "complete")
+	EXECUTE FUNCTION update_department_resource(NEW.id);
