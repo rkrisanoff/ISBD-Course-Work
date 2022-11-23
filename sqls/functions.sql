7e3196c7361ec57f6b9dd0b7f61203b93d794311
welllet <welllet@mail.ru>
2022-11-23 12:21:45 +0300

    added functions, triggers

--- sqls/functions.sql
+++ sqls/functions.sql
@@ -22,3 +22,31 @@ end if;
 return NEW;
 end;
 $$ LANGUAGE plpgsql;
+
+
+CREATE
+OR REPLACE FUNCTION task_complete_reward() RETURNS TRIGGER AS $$ begin 
+if 
+(
+	select
+		NEW.status
+) == 'complete'
+then
+UPDATE 
+department
+SET
+current_resource = current_resource + NEW.cost
+WHERE
+department.id = (
+	SELECT
+	department.id
+	FROM
+	task
+	JOIN post ON task.executor_post_id = post.id AND task.id = NEW.id
+	JOIN department ON post.department_id = department.id
+);
+end if;
+
+return NEW;
+end;
+$$ LANGUAGE plpgsql;
--- sqls/triggers.sql
+++ sqls/triggers.sql
@@ -1,3 +1,8 @@
-CREATE OR REPLACE TRIGGER limit_robot_hitpoints_t BEFORE
-INSERT
+CREATE OR REPLACE TRIGGER limit_robot_hitpoints_t 
+BEFORE INSERT
     ON robot FOR EACH ROW EXECUTE PROCEDURE "limit_robot_hitpoints"();
+
+
+CREATE OR REPLACE TRIGGER task_complete_reward_t 
+BEFORE INSERT OR UPDATE
+    ON task FOR EACH ROW EXECUTE PROCEDURE "task_complete_reward"();
