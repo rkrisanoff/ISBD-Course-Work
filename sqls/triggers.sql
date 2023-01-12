c7095bddde09ff079b1e9842cc30bbb37dd3fe74
welllet <welllet@mail.ru>
2023-01-12 05:12:13 +0300

    fix

--- sqls/triggers.sql
+++ sqls/triggers.sql
@@ -1,41 +1,41 @@
 set search_path to 's284712';
 CREATE TRIGGER limit_robot_hitpoints_t 
-BEFORE INSERT
+AFTER INSERT
     ON robot FOR EACH ROW EXECUTE PROCEDURE "limit_robot_hitpoints"();
 
 
 --CREATE TRIGGER task_complete_reward_t 
---BEFORE INSERT OR UPDATE
+--AFTER INSERT OR UPDATE
 --    ON task FOR EACH ROW EXECUTE PROCEDURE "task_complete_reward"();
 
 
 CREATE TRIGGER check_can_operate_robot_t
-BEFORE INSERT OR UPDATE
+AFTER INSERT OR UPDATE
     ON post FOR EACH ROW EXECUTE PROCEDURE "check_can_operate_robot"();
 
 
 CREATE TRIGGER body_creation_reward_t
-BEFORE INSERT
+AFTER INSERT
     ON body FOR EACH ROW EXECUTE PROCEDURE "body_creation_reward"();
 
 
 CREATE TRIGGER update_robots_brain_pay_t
-BEFORE UPDATE
+AFTER UPDATE
     ON robot FOR EACH ROW EXECUTE PROCEDURE "update_robots_brain_pay"();
 
 
 CREATE TRIGGER update_robots_eyes_pay_t
-BEFORE UPDATE
+AFTER UPDATE
     ON robot FOR EACH ROW EXECUTE PROCEDURE "update_robots_eyes_pay"();
 
 
 CREATE TRIGGER update_robots_body_pay_t
-BEFORE UPDATE
+AFTER UPDATE
     ON robot FOR EACH ROW EXECUTE PROCEDURE "update_robots_body_pay"();
 
 
 CREATE TRIGGER update_robots_hp_pay_t
-BEFORE UPDATE
+AFTER UPDATE
     ON robot FOR EACH ROW EXECUTE PROCEDURE "update_robots_hp_pay"();
 
 
@@ -45,7 +45,7 @@ AFTER INSERT OR UPDATE
 
 
 CREATE TRIGGER update_task_reward_t
-BEFORE INSERT OR UPDATE
+AFTER INSERT OR UPDATE
     ON task FOR EACH ROW EXECUTE PROCEDURE "update_task_reward"();
 
 
